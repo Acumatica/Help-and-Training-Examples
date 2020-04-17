6d6916b9c6c4af4b7639fd562e91de7b07402e4c
Ekaterina Androsova <ekaterina.androsova@acumatica.com>
2020-04-17 14:38:19 +0300

    AC-160896: Update T200 customization project files

--- Customization/T200/SourceFiles/DBScripts/T200_DatabaseTables.sql
+++ Customization/T200/SourceFiles/DBScripts/T200_DatabaseTables.sql
@@ -11,12 +11,12 @@ GO
 CREATE TABLE [dbo].[RSSVRepairService](
 	[CompanyID] [int] NOT NULL,
 	[ServiceID] [int] IDENTITY(1,1) NOT NULL,
+	[ServiceCD] [nvarchar](15) NOT NULL,
 	[Description] [nvarchar](50) NOT NULL,
 	[Active] [bit] NOT NULL,
 	[WalkInService] [bit] NOT NULL,
 	[PreliminaryCheck] [bit] NOT NULL,
 	[Prepayment] [bit] NOT NULL,
-	[ServiceCD] [nvarchar](15) NOT NULL,
 	[CreatedDateTime] [datetime] NOT NULL,
 	[CreatedByID] [uniqueidentifier] NOT NULL,
 	[CreatedByScreenID] [char](8) NOT NULL,
@@ -36,6 +36,13 @@ GO
 ALTER TABLE [dbo].[RSSVRepairService] ADD  CONSTRAINT [DF_RSSVRepairService_CompanyID]  DEFAULT ((0)) FOR [CompanyID]
 GO
 
+CREATE UNIQUE NONCLUSTERED INDEX [RSSVRepairService_ServiceCD_Uindex] ON [dbo].[RSSVRepairService]
+(
+	[CompanyID] ASC,
+	[ServiceCD] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
 
 /****** Object:  Table [dbo].[RSSVDevice]    Script Date: 25.04.2019 10:09:55 ******/
 SET ANSI_NULLS ON
@@ -49,6 +56,8 @@ CREATE TABLE [dbo].[RSSVDevice](
 	[DeviceID] [int] IDENTITY(1,1) NOT NULL,
 	[DeviceCD] [nvarchar](15) NOT NULL,
 	[Description] [nvarchar](256) NULL,
+	[Active] [bit] NOT NULL,
+	[AvgComplexityOfRepair] [char](1) NOT NULL,
 	[CreatedByID] [uniqueidentifier] NOT NULL,
 	[CreatedByScreenID] [char](8) NOT NULL,
 	[CreatedDateTime] [datetime] NOT NULL,
@@ -56,8 +65,6 @@ CREATE TABLE [dbo].[RSSVDevice](
 	[LastModifiedByScreenID] [char](8) NOT NULL,
 	[LastModifiedDateTime] [datetime] NOT NULL,
 	[tstamp] [timestamp] NOT NULL,
-	[AvgComplexityOfRepair] [nvarchar](1) NOT NULL,
-	[Active] [bit] NOT NULL,
 	[NoteID] [uniqueidentifier] NULL,
  CONSTRAINT [RSSVDevice_PK] PRIMARY KEY CLUSTERED 
 (
@@ -70,5 +77,9 @@ GO
 ALTER TABLE [dbo].[RSSVDevice] ADD  DEFAULT ((0)) FOR [CompanyID]
 GO
 
-
-
+CREATE UNIQUE NONCLUSTERED INDEX [RSSVDevice_DeviceCD_Uindex] ON [dbo].[RSSVDevice]
+(
+	[CompanyID] ASC,
+	[DeviceCD] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
