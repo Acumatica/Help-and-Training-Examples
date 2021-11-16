6d48abfc915d63a472b2b10fd504fdbfb09c0309
Petr Korchagin <Petr.Korchagin@acumatica.com>
2021-11-16 22:56:27 +0300

    AC-219346: the T280 customization project is uploaded

---
+++ Customization/T280/CodeSnippets/Step5.2/RSSVWorkOrderEntryTests.cs
@@ -0,0 +1,13 @@
+using PhoneRepairShop;
+using PX.Data;
+using PX.Tests.Unit;
+using PX.Objects.IN;
+using System.Linq;
+using Xunit;
+
+namespace PhoneRepairShop_Code.Tests
+{
+    public class RSSVWorkOrderEntryTests : TestBase
+    {
+    }
+}
