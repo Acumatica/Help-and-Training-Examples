f5883a675bb64318f908bcf50f38df92d6904b18
Kseniya Popova <kpopova@acumatica.com>
2024-03-01 17:22:41 +0100

    BS-3552: reverting latest changes

--- UnitTesting/T280/PhoneRepairShop_Code/PhoneRepairShop_Code.Tests/Properties/AssemblyInfo.cs
+++ UnitTesting/T280/PhoneRepairShop_Code/PhoneRepairShop_Code.Tests/Properties/AssemblyInfo.cs
@@ -2,6 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PhoneRepairShop_Code.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhoneRepairShop_Code.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -9,3 +21,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("52f7ec0f-1a91-4807-927e-840df4a55846")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
