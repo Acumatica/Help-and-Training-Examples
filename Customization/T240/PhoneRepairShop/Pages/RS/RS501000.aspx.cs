aae9d87e774dd935e137a21b38761dbd5b414846
Ekaterina Androsova <ekaterina.androsova@acumatica.com>
2024-04-21 20:57:12 -0400

    EDU-2228: Update customization project files for T240

--- Customization/T240/PhoneRepairShop/Pages/RS/RS501000.aspx.cs
+++ Customization/T240/PhoneRepairShop/Pages/RS/RS501000.aspx.cs
@@ -1,11 +1,4 @@
 using System;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.HtmlControls;
 
 public partial class  Page_RS501000 : PX.Web.UI.PXPage
 {
