64707ae481c3a9e0027d08050d6653818f47a733
Kseniya Popova <kpopova@acumatica.com>
2025-07-23 18:41:10 +0200

    EDU-7721: synchronizing changes from other T courses, T250 to Modern UI completed

---
+++ Customization/T190/SourceFiles/PhoneRepairShop/screens/RS/RS101000/RS101000.ts
@@ -0,0 +1,27 @@
+import {
+	PXScreen,
+	createSingle,
+	graphInfo,
+	PXView,
+	PXFieldState,
+	controlConfig,
+} from "client-controls";
+
+@graphInfo({
+	graphType: "PhoneRepairShop.RSSVSetupMaint",
+	primaryView: "Setup",
+})
+export class RS101000 extends PXScreen {
+	Setup = createSingle(RSSVSetup);
+}
+
+export class RSSVSetup extends PXView {
+	@controlConfig({allowEdit: true, })
+	NumberingID: PXFieldState;
+
+	@controlConfig({allowEdit: true, })
+	WalkInCustomerID: PXFieldState;
+
+	DefaultEmployee: PXFieldState;
+	PrepaymentPercent: PXFieldState;
+}
