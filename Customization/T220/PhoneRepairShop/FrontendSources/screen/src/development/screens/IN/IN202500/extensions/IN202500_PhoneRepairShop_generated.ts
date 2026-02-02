18a61e513a7713f4dbe61189a467dc61a8c13c17
Kseniya Popova <kpopova@acumatica.com>
2026-02-02 14:52:15 +0100

    EDU-8317: issue in T220 customization project fixed

--- Customization/T220/PhoneRepairShop/FrontendSources/screen/src/development/screens/IN/IN202500/extensions/IN202500_PhoneRepairShop_generated.ts
+++ Customization/T220/PhoneRepairShop/FrontendSources/screen/src/development/screens/IN/IN202500/extensions/IN202500_PhoneRepairShop_generated.ts
@@ -75,15 +75,15 @@ import {
 } from "client-controls";
 import { IN202500, ItemSettings } from "src/screens/IN/IN202500/IN202500";
 
-export interface IN202500_PhoneRepairShop3_generated extends IN202500 {}
-export class IN202500_PhoneRepairShop3_generated {
+export interface IN202500_PhoneRepairShop_generated extends IN202500 {}
+export class IN202500_PhoneRepairShop_generated {
   @gridConfig({ preset: GridPreset.Details })
   @viewInfo({ containerName: "Compatible Devices" })
   CompatibleDevices = createCollection(RSSVStockItemDevice);
 }
 
-export interface ItemSettings_PhoneRepairShop3_generated extends ItemSettings {}
-export class ItemSettings_PhoneRepairShop3_generated {
+export interface ItemSettings_PhoneRepairShop_generated extends ItemSettings {}
+export class ItemSettings_PhoneRepairShop_generated {
   @fieldInfo({ commitChanges: true })
   UsrRepairItem: PXFieldState;
 
