cde8ca09987b838be59384105946b00a21f492da
Kseniya Popova <kpopova@acumatica.com>
2026-02-09 09:34:42 +0100

    EDU-8317: added data views for the classic UI

--- ModernUI/T290/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVWorkOrderEntry.cs
+++ ModernUI/T290/PhoneRepairShop_Code/PhoneRepairShop_Code/RSSVWorkOrderEntry.cs
@@ -46,6 +46,20 @@ namespace PhoneRepairShop
             RSSVSetup setup = AutoNumSetup.Current;
         }
 
+        public PXFilter<MasterTable> MasterView;
+		public PXFilter<DetailsTable> DetailsView;
+
+		[Serializable]
+		public class MasterTable : PXBqlTable, IBqlTable
+		{
+
+		}
+
+		[Serializable]
+		public class DetailsTable : PXBqlTable, IBqlTable
+		{
+
+		}
         #endregion
 
         #region Event Handlers 
